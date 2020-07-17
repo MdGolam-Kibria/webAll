1666e81a0d8b463d37e241ef784a587d66b65ab9
Golam Kibria <61331272+MdGolam-Kibria@users.noreply.github.com>
2020-07-18 01:51:18 +0600

    Add files via upload

---
+++ sideBar/control.js
@@ -0,0 +1,8 @@
+function toggleSideBar(){
+    document.getElementById("sidebar")
+        .classList
+        .toggle("active")
+}
+function change(){
+    document.getElementById("text").innerHTML = "wow java script works";
+}
